709a4073d0c6e7fc37e395157fe6ef3d32af03d2
Fabricio M <fabricio2111@gmail.com>
2021-11-18 10:56:20 -0500

    product CRUD backend

--- mi_tiendecita/app/controllers/api/v1/products_controller.rb
+++ mi_tiendecita/app/controllers/api/v1/products_controller.rb
@@ -12,11 +12,45 @@ class Api::V1::ProductsController < ApiController
   end
 
   def create
+    @product = Product.new(product_params.merge({ store_id: params[:store_id] }))
+
+    if @product.save
+      render json: { product: @product }
+    else
+      render json: { errors: @product.errors.messages }
+    end
   end
 
   def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      render json: { product: @product }
+    else
+      render json: { errors: @product.errors.messages }
+    end
   end
 
   def destroy
+    @product = Product.find(params[:id])
+
+    if @product.destroy
+      render json: { message: 'Product deleted successfully' }
+    else
+      render json: { errors: @product.errors.messages }
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :price,
+      :code,
+      :quantity,
+      :description,
+      :image_url,
+    )
   end
 end

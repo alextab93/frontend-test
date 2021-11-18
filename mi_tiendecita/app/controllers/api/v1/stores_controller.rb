111d6288b78b9f5fbd1c206a18b791b0e333f383
Fabricio M <fabricio2111@gmail.com>
2021-11-18 11:20:07 -0500

    stores CRUD backend

--- mi_tiendecita/app/controllers/api/v1/stores_controller.rb
+++ mi_tiendecita/app/controllers/api/v1/stores_controller.rb
@@ -12,9 +12,31 @@ class Api::V1::StoresController < ApiController
   end
 
   def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      render json: { store: @store }
+    else
+      render json: { errors: @store.errors.messages }
+    end
   end
 
   def update
+    @store = Store.find(params[:id])
+
+    if @store.update(store_params)
+      render json: { store: @store }
+    else
+      render json: { errors: @store.errors.messages }
+    end
   end
 
+  private
+
+  def store_params
+    params.require(:store).permit(
+      :name,
+      :address
+    )
+  end
 end
--- mi_tiendecita/app/controllers/api_controller.rb
+++ mi_tiendecita/app/controllers/api_controller.rb
@@ -16,22 +16,22 @@ class ApiController < ActionController::Base
   end
 
   def decoded_token
-    if auth_header
-      token = auth_header.split(' ')[1]
-      # header: { 'Authorization': 'Bearer <token>' }
-      begin
-        JWT.decode(token, ENV['API_SECRET'], true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        nil
-      end
+    return unless auth_header
+
+    token = auth_header.split(' ')[1]
+    # header: { 'Authorization': 'Bearer <token>' }
+    begin
+      JWT.decode(token, ENV['API_SECRET'], true, algorithm: 'HS256')
+    rescue JWT::DecodeError
+      nil
     end
   end
 
   def logged_in_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
-    end
+    return unless decoded_token
+
+    user_id = decoded_token[0]['user_id']
+    @user = User.find_by(id: user_id)
   end
 
   def logged_in?
